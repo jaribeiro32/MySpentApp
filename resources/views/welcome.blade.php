055fd07c428a38a3d560d2be4de0d5f8f37ffebf
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-11-24 17:29:15 -0300

    Expense Create Save and Validation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,12 @@
                 font-family: 'Nunito';
             }
         </style>
+        @livewireStyles
     </head>
     <body class="antialiased">
 
     <livewire:expense.expense-create></livewire:expense.expense-create>
+    @livewireScripts
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
