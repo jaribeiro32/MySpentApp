bb59ce721863cb6aa1838c586648801721b84f99
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-01-13 14:03:24 -0300

    expenses photo upload

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -4,35 +4,46 @@ namespace App\Http\Livewire\Expense;
 
 use App\Models\Expense;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ExpenseCreate extends Component
 {
+
+    use WithFileUploads;
+
     public $amount;
     public $type;
     public $description;
+    public $photo;
 
     protected $rules = [
         'description' => 'required',
         'amount' => 'required',
         'type' => 'required',
+        'photo' => 'image',
     ];
 
     public function createExpense()
     {
         $this->validate();
 
+        if ($this->photo) {
+            $photo = $this->photo->store('expenses-photos', 'public');
+        }
+
         auth()->user()->expenses()->create([
             'description' => $this->description,
             'amount' => $this->amount,
             'type' => $this->type,
             'user_id' => 1,
+            'photo' => $photo ?? null,
         ]);
 
         session()->flash('message', 'Registro criado com sucesso!');
 
         $this->description = null;
         $this->amount = null;
-        $this->type=null;
+        $this->type = null;
     }
 
     public function render()
